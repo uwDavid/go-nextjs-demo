c9f8c1d26dfdc45bb7f50f1209068a126c34275a
uwDavid <uwDavid@users.noreply.github.com>
2023-10-10 19:06:16 -0400

    connect front-end

--- backend/api/server.go
+++ backend/api/server.go
@@ -7,6 +7,7 @@ import (
 	db "nextjs/backend/db/sqlc"
 	"nextjs/backend/utils"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
@@ -37,6 +38,8 @@ func NewServer(envPath string) *Server {
 
 	// Initialize Gin
 	g := gin.Default()
+
+	g.Use(cors.Default())
 
 	return &Server{
 		queries: q,
--- backend/go.mod
+++ backend/go.mod
@@ -3,6 +3,7 @@ module nextjs/backend
 go 1.21.1
 
 require (
+	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.9.1
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/lib/pq v1.10.9
